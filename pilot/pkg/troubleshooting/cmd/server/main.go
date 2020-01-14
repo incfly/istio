9e309ab67da40f862f46cc6fbf5ede1537e1fb98
Jianfei Hu <jianfeih@google.com>
2020-01-14 06:04:56 +0000

    initial copy of apiserver from yan repo.

--- pilot/pkg/troubleshooting/cmd/server/main.go
+++ pilot/pkg/troubleshooting/cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -14,6 +15,7 @@ var (
 	rootCmd cobra.Command = cobra.Command{
 		Use: "agent, to be replaced by pilot agent",
 		Run: func(cmd *cobra.Command, args []string) {
+			go startAPIService()
 			fmt.Println("istiod started")
 			s, err := ts.NewServer(&cfg)
 			if err != nil {
@@ -25,6 +27,25 @@ var (
 		},
 	}
 )
+
+func startAPIService() {
+	log.Infof("starting api server on port 9000")
+	// starting istioctl facing echo service in front of apiserver.
+	srv := &http.Server{Addr: ":9000", Handler: http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			// Log the request protocol
+			log.Infof("Got connection: %s", r.Proto)
+			// Send a message back to the client
+			w.Write([]byte("Hello"))
+		})}
+
+	// Start the server with TLS, since we are running HTTP/2 it must be
+	// run with TLS. Exactly how you would run an HTTP/1.1 server with TLS connection.
+	log.Infof("Serving on https://0.0.0.0:9000")
+	if err := srv.ListenAndServeTLS("server.crt", "server.key"); err != nil {
+		log.Fatalf("failed to start echo service for apiserver %v", err)
+	}
+}
 
 func init() {
 	rootCmd.PersistentFlags().Uint32VarP(
--- pilot/pkg/troubleshooting/setup.sh
+++ pilot/pkg/troubleshooting/setup.sh
@@ -40,6 +40,10 @@ cli() {
   go run ./cmd/cli "$@"
 }
 
+genhttp2cert() {
+  openssl req -newkey rsa:2048 -nodes -keyout server.key -x509 -days 365 -out server.crt
+}
+
 
 # out of order execution
 # starting with 1
