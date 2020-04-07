d1a6bff18a719a6bb58c6060c83a848016a05f6c
Jianfei Hu <jianfeih@google.com>
2020-04-07 06:29:24 +0000

    more test, sorting event and clone proto.

--- galley/pkg/config/processor/transforms/jwks/create_test.go
+++ galley/pkg/config/processor/transforms/jwks/create_test.go
@@ -1,8 +1,10 @@
 package jwks
 
 import (
+	"sort"
 	"testing"
 
+	"github.com/gogo/protobuf/proto"
 	"github.com/google/go-cmp/cmp"
 	secv1 "istio.io/api/security/v1beta1"
 	"istio.io/istio/galley/pkg/config/processing"
@@ -24,16 +26,45 @@ var (
 				},
 			},
 		},
-		"A": &secv1.RequestAuthentication{
+		"a-jwks-filled": &secv1.RequestAuthentication{
 			JwtRules: []*secv1.JWTRule{
 				&secv1.JWTRule{
 					Issuer: "a-iss",
-					Jwks:   "a-pubkey",
+					Jwks:   "a-pubkey-filled",
+				},
+			},
+		},
+		"b": &secv1.RequestAuthentication{
+			JwtRules: []*secv1.JWTRule{
+				&secv1.JWTRule{
+					Issuer:  "b-iss",
+					JwksUri: "b-uri",
+				},
+			},
+		},
+		"a-from-header-foo": &secv1.RequestAuthentication{
+			JwtRules: []*secv1.JWTRule{
+				&secv1.JWTRule{
+					Issuer:  "a-iss",
+					JwksUri: "a-uri",
+					FromHeaders: []*secv1.JWTHeader{
+						&secv1.JWTHeader{
+							Name: "x-foo",
+						},
+					},
 				},
 			},
 		},
 	}
 )
+
+func policyByName(name string) *secv1.RequestAuthentication {
+	p, ok := policyRegistry[name]
+	if !ok {
+		return nil
+	}
+	return proto.Clone(p).(*secv1.RequestAuthentication)
+}
 
 type fakeJwksresolver struct {
 	jwksMap  map[string]string
@@ -82,10 +113,35 @@ func (fh *fakeHandler) Handle(e event.Event) {
 	fh.events = append(fh.events, &e)
 }
 
+// ByAge implements sort.Interface based on the Age field.
+type byResourceName []*event.Event
+
+func (a byResourceName) Len() int { return len(a) }
+func (a byResourceName) Less(i, j int) bool {
+	return a[i].Resource.Metadata.FullName.String() < a[j].Resource.Metadata.FullName.String()
+}
+func (a byResourceName) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+
 func (fh *fakeHandler) validateEvents(t *testing.T, events []*event.Event) {
 	t.Helper()
+	// We sort the events because the order of the updates of individual policy does not matter.
+	sort.Sort(byResourceName(events))
+	sort.Sort(byResourceName(fh.events))
 	if diff := cmp.Diff(fh.events, events); diff != "" {
 		t.Errorf("handler received different envents, diff %v", diff)
+	}
+}
+
+func policyAddEvent(t *testing.T, p *secv1.RequestAuthentication) *event.Event {
+	if p == nil {
+		t.Fatalf("unexpected ipnut nil policy")
+	}
+	return &event.Event{
+		Kind:   event.Added,
+		Source: collections.IstioSecurityV1Beta1Requestauthentications,
+		Resource: &resource.Instance{
+			Message: p,
+		},
 	}
 }
 
@@ -103,18 +159,11 @@ func TestJwksTransformer(t *testing.T) {
 			name: "BasicTransform",
 			initial: transformState{
 				jwksMap: map[string]string{
-					"a-uri": "a-pubkey",
+					"a-uri": "a-pubkey-by-resolver",
 				},
 			},
 			updates: jwksUpdate{
-				// Add single policy for "a".
-				policyEvent: &event.Event{
-					Kind:   event.Added,
-					Source: collections.IstioSecurityV1Beta1Requestauthentications,
-					Resource: &resource.Instance{
-						Message: policyRegistry["a"],
-					},
-				},
+				policyEvent: policyAddEvent(t, policyByName("a")),
 			},
 			want: []*event.Event{
 				&event.Event{
@@ -126,7 +175,34 @@ func TestJwksTransformer(t *testing.T) {
 								&secv1.JWTRule{
 									Issuer:  "a-iss",
 									JwksUri: "a-uri",
-									Jwks:    "a-pubkey",
+									Jwks:    "a-pubkey-by-resolver",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "OriginalJwksRespectEvenUpdated",
+			initial: transformState{
+				jwksMap: map[string]string{
+					"a-uri": "a-pubkey-by-resolver",
+				},
+			},
+			updates: jwksUpdate{
+				policyEvent: policyAddEvent(t, policyByName("a-jwks-filled")),
+			},
+			want: []*event.Event{
+				&event.Event{
+					Kind:   event.Added,
+					Source: collections.IstioSecurityV1Beta1Requestauthentications,
+					Resource: &resource.Instance{
+						Message: &secv1.RequestAuthentication{
+							JwtRules: []*secv1.JWTRule{
+								&secv1.JWTRule{
+									Issuer: "a-iss",
+									Jwks:   "a-pubkey-filled",
 								},
 							},
 						},
@@ -136,19 +212,97 @@ func TestJwksTransformer(t *testing.T) {
 		},
 		{
 			name: "IgnoreResolverEmptyResponse",
-			// want: []*event.Event{},
+			initial: transformState{
+				jwksMap: map[string]string{
+					"a-uri": "",
+				},
+			},
+			updates: jwksUpdate{
+				policyEvent: policyAddEvent(t, policyByName("a")),
+			},
+			want: []*event.Event{
+				&event.Event{
+					Kind:   event.Added,
+					Source: collections.IstioSecurityV1Beta1Requestauthentications,
+					Resource: &resource.Instance{
+						Message: &secv1.RequestAuthentication{
+							JwtRules: []*secv1.JWTRule{
+								&secv1.JWTRule{
+									Issuer:  "a-iss",
+									JwksUri: "a-uri",
+									Jwks:    "", // leave it as empty, unmodified.
+								},
+							},
+						},
+					},
+				},
+			},
 		},
 		{
-			name: "OriginalJwksRespectEvenUpdated",
-		},
-		{
-			name: "PolicyAddedTransform",
-		},
-		{
-			name: "JwksUpdate",
-		},
-		{
-			name: "JwksUpdateOnlyAffected",
+			name: "BasicTransformByRefresh",
+			initial: transformState{
+				jwksMap: map[string]string{
+					"a-uri": "a-pubkey-by-resolver-v1",
+				},
+				policies: map[string]*secv1.RequestAuthentication{
+					"a":                 policyByName("a"),
+					"b":                 policyByName("b"),
+					"a-from-header-foo": policyByName("a-from-header-foo"),
+				},
+			},
+			updates: jwksUpdate{
+				jwksUpdate: &jwksEntry{
+					jwksURI: "a-uri",
+					jwks:    "a-pubkey-by-resolver-v2",
+				},
+			},
+			want: []*event.Event{
+				&event.Event{
+					Kind:   event.Updated,
+					Source: collections.IstioSecurityV1Beta1Requestauthentications,
+					Resource: &resource.Instance{
+						Metadata: resource.Metadata{
+							FullName: resource.FullName{
+								Name: resource.LocalName("a"),
+							},
+						},
+						Message: &secv1.RequestAuthentication{
+							JwtRules: []*secv1.JWTRule{
+								&secv1.JWTRule{
+									Issuer:  "a-iss",
+									JwksUri: "a-uri",
+									Jwks:    "a-pubkey-by-resolver-v2",
+								},
+							},
+						},
+					},
+				},
+				&event.Event{
+					Kind:   event.Updated,
+					Source: collections.IstioSecurityV1Beta1Requestauthentications,
+					Resource: &resource.Instance{
+						Metadata: resource.Metadata{
+							FullName: resource.FullName{
+								Name: resource.LocalName("a-from-header-foo"),
+							},
+						},
+						Message: &secv1.RequestAuthentication{
+							JwtRules: []*secv1.JWTRule{
+								&secv1.JWTRule{
+									Issuer:  "a-iss",
+									JwksUri: "a-uri",
+									Jwks:    "a-pubkey-by-resolver-v2",
+									FromHeaders: []*secv1.JWTHeader{
+										&secv1.JWTHeader{
+											Name: "x-foo",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
 		},
 		{
 			name: "PolicyDeletionAndJwksUpdate",
@@ -164,11 +318,16 @@ func TestJwksTransformer(t *testing.T) {
 			}
 			// init the state.
 			xform := newJwksTransformer(res, processing.ProcessorOptions{})
-			for _, p := range c.initial.policies {
+			for k, p := range c.initial.policies {
 				xform.Handle(event.Event{
 					Kind:   event.Added,
 					Source: collections.IstioSecurityV1Beta1Requestauthentications,
 					Resource: &resource.Instance{
+						Metadata: resource.Metadata{
+							FullName: resource.FullName{
+								Name: resource.LocalName(k),
+							},
+						},
 						Message: p,
 					},
 				})
