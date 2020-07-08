9f7ad8b00118dcaa085bcfb9c9f25be4fe951252
Oliver Liu <yonggangl@google.com>
2020-07-08 14:20:30 -0700

    Initial client auth for XDS.
    Cherry-pick of
    https://gke-internal.googlesource.com/istio/istio/+/b1d624563abe3d319fd4f19e3e58bcfc2a31a852.
    
    Change-Id: Ib518911647f3598621076b7d892422c88c7cda71

--- pilot/pkg/proxy/envoy/v2/discovery.go
+++ pilot/pkg/proxy/envoy/v2/discovery.go
@@ -27,6 +27,7 @@ import (
 	"istio.io/istio/pilot/pkg/features"
 	"istio.io/istio/pilot/pkg/model"
 	"istio.io/istio/pilot/pkg/networking/core"
+	"istio.io/istio/security/pkg/server/ca/authenticate"
 )
 
 var (
@@ -124,6 +125,9 @@ type DiscoveryServer struct {
 	adsClientsMutex sync.RWMutex
 
 	StatusReporter DistributionStatusCache
+
+	// Authenticators for XDS requests. Should be same/subset of the CA authenticators.
+	Authenticators []authenticate.Authenticator
 }
 
 // EndpointShards holds the set of endpoint shards of a service. Registries update
@@ -159,6 +163,17 @@ func NewDiscoveryServer(env *model.Environment, plugins []string) *DiscoveryServ
 		DebugConfigs:            features.DebugConfigs,
 		debugHandlers:           map[string]string{},
 		adsClients:              map[string]*XdsConnection{},
+	}
+
+	if features.XDSAuth {
+		// This is equivalent with the mTLS authentication for workload-to-workload.
+		// The GRPC server is configured in bootstrap.initSecureDiscoveryService, using the root
+		// certificate as 'ClientCAs'. To accept additional signers for client identities - add them
+		// there, will be used for CA signing as well.
+		out.Authenticators = append(out.Authenticators, &authenticate.ClientCertAuthenticator{})
+
+		// TODO: we may want to support JWT/OIDC auth as well - using the same list of auth as
+		// CA. Will require additional refactoring - probably best for 1.7.
 	}
 
 	// Flush cached discovery responses when detecting jwt public key change.
